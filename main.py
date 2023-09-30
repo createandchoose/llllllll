5f5c72dedbbb3b00bcb0e592695675472e8e6ea1
m4xsc0r3 <maxscoreofficia1820@gmail.com>
2023-09-30 11:13:32 +0900

    asdasd

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ def index():
 
     soup = BeautifulSoup(html_page, 'html.parser')
     href_profile = [a.get('href') for a in soup.find_all('a', class_='pic')]
+
     img_profile = [img.get('src').replace('_42.jpg', '_23.jpg') for img in soup.find_all('img', class_='thumb')]
     name_profile = [div.text for div in soup.find_all('div', {'class': 'name'})]
+
 
     albums = []
     for i in range(len(name_profile)):
