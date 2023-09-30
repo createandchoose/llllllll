b6d9cfff611cbd55d71b9234cdbbac613cf87abe
m4xsc0r3 <maxscoreofficia1820@gmail.com>
2023-09-30 11:12:47 +0900

    asdas

--- main-clone.py
+++ main-clone.py
@@ -7,13 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     html_page = requests.get("https://thefoxxstuff.bandcamp.com/album/one-soul").content
-    
+
     soup = BeautifulSoup(html_page, 'html.parser')
-
     href_profile = [a.get('href') for a in soup.find_all('a', class_='pic')]
-
     img_profile = [img.get('src').replace('_42.jpg', '_23.jpg') for img in soup.find_all('img', class_='thumb')]
-
     name_profile = [div.text for div in soup.find_all('div', {'class': 'name'})]
 
     albums = []
